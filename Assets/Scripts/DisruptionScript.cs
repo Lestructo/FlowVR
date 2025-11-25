ab72829630568db7965f6a31248da526db8a27bf
jwrodriguez <jwrodriguez@ou.edu>
2025-11-24 19:27:25 -0600

    Disruiption v1

---
+++ Assets/Scripts/DisruptionScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class DisruptionScript : MonoBehaviour
+{
+    public GameObject racketRoot;
+    private Collider ballCollider;
+    private Collider[] racketColliders;
+    private bool timerStarted = false;
+
+    private void Start()
+    {
+        ballCollider = GetComponent<Collider>();
+        racketColliders = racketRoot.GetComponentsInChildren<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == racketRoot && !timerStarted)
+        {
+            timerStarted = true;
+            StartCoroutine(TimerRoutine());
+        }
+    }
+
+    IEnumerator TimerRoutine()
+    {
+        yield return new WaitForSeconds(5f);
+
+        foreach (var col in racketColliders)
+            Physics.IgnoreCollision(ballCollider, col, true);
+
+        yield return new WaitForSeconds(20f);
+
+        foreach (var col in racketColliders)
+            Physics.IgnoreCollision(ballCollider, col, false);
+    }
+}
